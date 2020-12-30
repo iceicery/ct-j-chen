17f7f0c50dc0b5bb276120b6b97b4dbcdd0faaa6
pinwei wu <pw.amy.wu@gmail.com>
2020-12-29 23:14:45 -0600

    add header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
-import avatar from '../../images/avatar.jpg';
 import './Header.css';
 export default function Header() {
     return (
         <section className="header">
-
+            <div className="header__text-box">
+                <h3>Hi, I am Chien-Ting Chen,</h3>
+                <h3>USRA Research&nbsp;Scientist at NASA MSFC.</h3>
+            </div>
+            <div className="header__button-box">
+                <button className="header__button">About Me</button>
+                <button className="header__button">News Update</button>
+                <button className="header__button">Recent Work</button>
+            </div>
         </section>
     )
 }
