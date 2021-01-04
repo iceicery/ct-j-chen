eb4f8aec54638643cd9fb32c7fd77b5978501e88
pinwei wu <pw.amy.wu@gmail.com>
2021-01-03 23:12:54 -0600

    adjust react players size

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -11,10 +11,12 @@ export default function News() {
                         <p className="news__text">05/16/2020:</p>
                         <p className="news__text">In U.S. Space & Rocket Center interview, Joseph talks with Dr. Chien-Ting Chen from NASA-MARSHALL about supermassive black holes, how they impact the galaxies they're in and what happens when you don't ask questions.</p>
                     </div>
-                    <ReactPlayer url="https://www.youtube.com/watch?v=TiKltavBz1U" />
+                    <div className="news__player-box">
+                        <ReactPlayer className="news__player" width="100%" height="60%" url="https://www.youtube.com/watch?v=TiKltavBz1U" />
+                    </div>
                 </li>
                 <li className="news__list news__list_flex">
-                    <div>
+                    <div className="news__text-box">
                         <p className="news__text">07/13/2019:</p>
                         <p className="news__text">the Russian-German X-ray space telescope Spektr-RG was recently launched on July 13th! See&nbsp;<a className="news__link" target="_blank" rel="noreferrer" href="https://wwwastro.msfc.nasa.gov/artxc/docs/launch.html">this&nbsp;webpage</a> for more information.</p>
                     </div>
